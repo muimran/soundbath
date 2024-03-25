427835f86e96b3ab3e18f9135774a3b1cd4c6305
Muhammad Imran <44212121+muimran@users.noreply.github.com>
2024-03-25 20:28:06 +0000

    Update script.js

--- web/script.js
+++ web/script.js
@@ -66,7 +66,7 @@ function updateAverageRainfall() {
 // Map setup and data fetching
 map.on('load', () => {
     // Fetch GeoJSON data
-    fetch('https://raw.githubusercontent.com/muimran/test-map/main/web/data/myData.geojson')
+    fetch('https://raw.githubusercontent.com/muimran/soundbath/main/web/data/myData.geojson')
         .then(response => response.json())
         .then(data => {
             geojsonData = data;
